669210fc55c983c5eb827b1d2e603c6fa01f9193
Морозов Иван Денисович <i.morozov@nicetu.spb.ru>
2023-08-06 17:43:31 +0300

    add models

--- testcase/blogs/models.py
+++ testcase/blogs/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Blog(models.Model):
+    username = models.SlugField(
+        unique=True,
+        max_length=20,
+    )
+
+
 class Post(models.Model):
     test = models.TextField(max_length=140)
     title = models.CharField(
@@ -13,11 +20,23 @@ class Post(models.Model):
     is_read = models.BooleanField(
         default=False
     )
+    blog = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
 
 
-class Blog(models.Model):
-    username = models.SlugField(
-
+class Follow(models.Model):
+    followed = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        related_name='author',
+    )
+    follower = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        related_name='follower'
     )
 
 
