e35a44d00c2bb108b75b46baa7f8381a4a5d2365
Морозов Иван Денисович <i.morozov@nicetu.spb.ru>
2023-08-07 15:37:58 +0300

    add celery task

--- testcase/testcase/celery.py
+++ testcase/testcase/celery.py
@@ -1,9 +1,16 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testcase.settings")
 app = Celery("test_case")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'send-five-posts-every-day': {
+        'task': 'blogs.tasks.send_last_posts',
+        'schedule': crontab(minute=0, hour='*/24')
+    }
+}
