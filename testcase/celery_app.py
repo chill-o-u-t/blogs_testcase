bc8a1dbf2089f45a9f93313ddd81f3c6cdf3fb03
Морозов Иван Денисович <i.morozov@nicetu.spb.ru>
2023-08-07 17:57:05 +0300

    fix 3.0

--- testcase/celery_app.py
+++ testcase/celery_app.py
@@ -3,10 +3,12 @@ import os
 
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testcase.settings")
 app = Celery("testcase")
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings")
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
